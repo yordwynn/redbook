305928164c6587882a145015b620c73353a8aad0
yordwynn <uPn3hv@r>
2020-11-06 00:53:22 +0500

    set head

--- exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -1,9 +1,9 @@
 package fpinscala.datastructures
 
-import fpinscala.datastructures.List.tail
+import fpinscala.datastructures.List.{setHead, tail}
 import org.scalatest.flatspec.AnyFlatSpec
 
-class ListTest extends AnyFlatSpec{
+class ListTest extends AnyFlatSpec {
   "ex 3.1" should "return 3" in {
     assert(List.x == 3)
   }
@@ -11,6 +11,12 @@ class ListTest extends AnyFlatSpec{
   "tail" should "handle arbitrary lists" in {
     assert(tail(Nil) == Nil)
     assert(tail(List(3)) == Nil)
-    assert(tail(List(1,2,3) ) == List(2,3))
+    assert(tail(List(1, 2, 3)) == List(2, 3))
+  }
+
+  "list" should "handle set head" in {
+    assert(setHead(Nil, 1) == Nil)
+    assert(setHead(List(2), 1) == List(1))
+    assert(setHead(List(3, 2), 1) == List(1, 2))
   }
 }
