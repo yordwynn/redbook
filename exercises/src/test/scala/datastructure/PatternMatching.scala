2819ae23cec36c6cab2c6ef591ab192010aa26e3
yordwynn <uPn3hv@r>
2020-11-06 00:28:40 +0500

    ex 3.1

---
+++ exercises/src/test/scala/datastructure/PatternMatching.scala
@@ -0,0 +1,10 @@
+package datastructure
+
+import fpinscala.datastructures.List
+import org.scalatest.flatspec.AnyFlatSpec
+
+class PatternMatching extends AnyFlatSpec{
+  "ex 3.1" should "return 3" in {
+    assert(List.x == 3)
+  }
+}
