0ecd90d7bba3b76663034dfac31e866811a1ca2e
yordwynn <uPn3hv@r>
2020-12-26 21:52:48 +0500

    fold for tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -13,28 +13,40 @@ object Tree {
         }
     }
 
+    def sizeViaFold[A](tree: Tree[A]): Int = fold(tree)(_ => 1)((l, r) => 1 + l + r)
+
     def maximum(intTree: Tree[Int]): Int = {
         intTree match {
             case Leaf(v) => v
-            case Branch(left, right) => maximum(left).max(maximum(right))
+            case Branch(left, right) => maximum(left) max maximum(right)
         }
     }
+
+    def maximumViaFold(intTree: Tree[Int]): Int = fold(intTree)(a => a)((l, r) => l max r)
 
     def depth[A](tree: Tree[A]): Int = {
-        def go(tr: Tree[A], dpth: Int): Int = {
-            tr match {
-                case Leaf(_) => dpth
-                case Branch(left, right) => go(left, dpth + 1).max(go(right, dpth + 1))
-            }
+        tree match {
+            case Leaf(_) => 0
+            case Branch(left, right) => 1 + depth(left) max depth(right)
         }
-
-        go(tree, 0)
     }
+
+    def depthViaFold[A](tree: Tree[A]): Int = fold(tree)(_ => 0)((l, r) => 1 + l max r)
 
     def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = {
         tree match {
             case Leaf(value) => Leaf(f(value))
             case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+        }
+    }
+
+    def mapViaFold[A, B](tree: Tree[A])(f: A => B): Tree[B] = 
+        fold(tree)(a => Leaf(f(a)): Tree[B])((l, r) => Branch(l, r))
+
+    def fold[A, B](tree: Tree[A])(f: A => B)(g: (B, B) => B): B = {
+        tree match {
+            case Leaf(a) => f(a)
+            case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
         }
     }
 }
