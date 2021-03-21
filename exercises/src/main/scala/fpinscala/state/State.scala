a1c32fafe9ab4eb04e82b02392f3b886d0b681b4
Yury Egorov <yurij.a.egorov@gmail.com>
2021-03-22 00:01:59 +0500

    exercise 6.2

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -1,6 +1,5 @@
 package fpinscala.state
 
-
 trait RNG {
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
 }
@@ -24,7 +23,7 @@ object RNG {
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
     rng => {
       val (a, rng2) = s(rng)
       (f(a), rng2)
@@ -38,27 +37,33 @@ object RNG {
     }
   }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    nonNegativeInt(rng) match {
+      case (0, state) => (1.0 / Int.MaxValue, state)
+      case (Int.MaxValue, state) => ((Int.MaxValue - 1).toDouble / Int.MaxValue, state)
+      case (intVal, state) => (intVal.toDouble / Int.MaxValue, state)
+    }
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int, Double), RNG) = ???
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = ???
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = ???
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
 
-case class State[S,+A](run: S => (A, S)) {
+case class State[S, +A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
     ???
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     ???
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     ???
--- exercises/src/test/scala/fpinscala/state/StateTest.scala
+++ exercises/src/test/scala/fpinscala/state/StateTest.scala
@@ -3,8 +3,13 @@ package fpinscala.state
 import org.scalatest.flatspec.AnyFlatSpec
 
 class StateTest extends AnyFlatSpec {
-    "RNG#nonNegativeInt" should "return non negative int" in {
-        val (value, _) = RNG.nonNegativeInt(RNG.Simple(0))
-        assert(value >= 0)
-    }
+  "RNG#nonNegativeInt" should "return non negative int" in {
+    val (value, _) = RNG.nonNegativeInt(RNG.Simple(0))
+    assert(value >= 0)
+  }
+
+  "RNG#double" should "return double from the interval (0, 1)" in {
+    val (value, _) = RNG.double(RNG.Simple(0))
+    assert(0 < value && value < 1)
+  }
 }
