986ffa55eaa66e3d76786ed57f00a1a5f28e167d
Yury Egorov <yurij.a.egorov@gmail.com>
2021-04-14 00:22:17 +0500

    tests for exercises 7.5 and 7.6

--- exercises/src/test/scala/fpinscala/parallelism/ParTest.scala
+++ exercises/src/test/scala/fpinscala/parallelism/ParTest.scala
@@ -11,4 +11,18 @@ class ParTest extends AnyFlatSpec {
         assertThrows[TimeoutException](Par.run(pool)(Par.map2(par1, par2, 10, TimeUnit.MILLISECONDS)(_ + _)).get())
         assert(Par.run(pool)(Par.map2(par1, par2, 100, TimeUnit.MILLISECONDS)(_ + _)).get() == 25)
     }
+
+    "sequence" should "work" in {
+        val pool = Executors.newFixedThreadPool(5)
+        val expected = Par.lazyUnit(List(1, 2, 3))
+        val seq = List(Par.lazyUnit(1), Par.lazyUnit(2), Par.lazyUnit(3))
+        assert(Par.equal(pool)(expected, Par.sequence(seq)))
+    }
+
+    "parFilter" should "work" in {
+        val pool = Executors.newFixedThreadPool(5)
+        val expected = Par.lazyUnit(List(1, 2, 3))
+        val seq = List(1, 2, 3, 4, 5)
+        assert(Par.equal(pool)(expected, Par.parFilter(seq)(_ < 4)))
+    }
 }
