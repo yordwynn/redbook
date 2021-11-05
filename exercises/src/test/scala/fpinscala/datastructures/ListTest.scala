66d649ef44689fcb90a150a1ceb79bbd03d285b2
yordwynn <yurij.a.egorov@gmail.com>
2021-11-06 00:29:38 +0500

    Ex 3.6

--- exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -19,43 +19,43 @@ class ListTest extends AnyFlatSpec {
   "list" should "handle drop" in {
     assert(
       drop(Nil, 0) == Nil,
-      "drop of zero elements from empty list is empty list"
+      "drop of zero elements from empty list is empty list",
     )
     assert(
       drop(Nil, 1) == Nil,
-      "drop of one element from empty list is empty list"
+      "drop of one element from empty list is empty list",
     )
     assert(
       drop(Nil, 10) == Nil,
-      "drop of many elements from empty list is empty list"
+      "drop of many elements from empty list is empty list",
     )
     assert(
       drop(List(3), 0) == List(3),
-      "drop of zero elements from single-element list is the list"
+      "drop of zero elements from single-element list is the list",
     )
     assert(
       drop(List(3), 1) == Nil,
-      "drop of one element from single-element list is empty list"
+      "drop of one element from single-element list is empty list",
     )
     assert(
       drop(List(3), 10) == Nil,
-      "drop of many elements from single-element list is empty list"
+      "drop of many elements from single-element list is empty list",
     )
     assert(
       drop(List(1, 2, 3), 0) == List(1, 2, 3),
-      "drop of zero elements from list is list"
+      "drop of zero elements from list is list",
     )
     assert(
       drop(List(1, 2, 3), 1) == List(2, 3),
-      "drop of one elements from list is list without 1st element"
+      "drop of one elements from list is list without 1st element",
     )
     assert(
       drop(List(1, 2, 3), 2) == List(3),
-      "drop of n elements from list is list without 1st n elements"
+      "drop of n elements from list is list without 1st n elements",
     )
     assert(
       drop(List(1, 2, 3), 10) == Nil,
-      "drop of too many elements from list is empty list"
+      "drop of too many elements from list is empty list",
     )
   }
 
@@ -63,31 +63,43 @@ class ListTest extends AnyFlatSpec {
     val positive = (x: Int) => x > 0
     assert(
       dropWhile(Nil, positive) == Nil,
-      "dropWhile of empty list should be empty list"
+      "dropWhile of empty list should be empty list",
     )
     assert(
       dropWhile(List(1), positive) == Nil,
-      "dropWhile of list with single valid element should be empty list"
+      "dropWhile of list with single valid element should be empty list",
     )
     assert(
       dropWhile(List(1, 2, 3, 4), positive) == Nil,
-      "dropWhile of list with only valid elements should be empty list"
+      "dropWhile of list with only valid elements should be empty list",
     )
     assert(
       dropWhile(List(1, 2, -3, 4), positive) == List(-3, 4),
-      "dropWhile of list with two leading valid elements should be list without leading elements"
+      "dropWhile of list with two leading valid elements should be list without leading elements",
     )
     assert(
       dropWhile(List(1, -2, -3, 4), positive) == List(-2, -3, 4),
-      "dropWhile of list with one leading valid element should be list without leading element"
+      "dropWhile of list with one leading valid element should be list without leading element",
     )
     assert(
       dropWhile(List(-1, -2, -3, 4), positive) == List(-1, -2, -3, 4),
-      "dropWhile of list with no leading valid elements should be same list"
+      "dropWhile of list with no leading valid elements should be same list",
     )
     assert(
       dropWhile(List(-1, -2, -3, -4), positive) == List(-1, -2, -3, -4),
-      "dropWhile of list with no valid elements should be Nil"
+      "dropWhile of list with no valid elements should be Nil",
+    )
+  }
+
+  "list" should "handle init" in {
+    assert(init(Nil) == Nil, "init of empty list should be empty list")
+    assert(
+      init(List(3)) == Nil,
+      "init of single-element-list should be empty list",
+    )
+    assert(
+      init(List(1, 2, 3)) == List(1, 2),
+      "init of list should not have last element",
     )
   }
 }
