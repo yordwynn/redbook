fc5048e011a6e779f4f429f8fc17c9968f4dd55c
yordwynn <yurij.a.egorov@gmail.com>
2021-11-06 00:09:20 +0500

    Ex 3.3

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -57,7 +57,12 @@ object List { // `List` companion object. Contains functions for creating and wo
     }
   }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(head, tail) => Cons(h, tail)
+    }
+  }
 
   def drop[A](l: List[A], n: Int): List[A] = ???
 
--- exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -1,12 +1,18 @@
 package fpinscala.datastructures
 
 import org.scalatest.flatspec.AnyFlatSpec
-import fpinscala.datastructures.List.tail
+import fpinscala.datastructures.List.{tail, setHead}
 
 class ListTest extends AnyFlatSpec {
   "tail" should "handle arbitrary lists" in {
     assert(tail(Nil) == Nil)
     assert(tail(List(3)) == Nil)
     assert(tail(List(1, 2, 3)) == List(2, 3))
+  }
+
+  "list" should "handle set head" in {
+    assert(setHead(Nil, 1) == Nil)
+    assert(setHead(List(2), 1) == List(1))
+    assert(setHead(List(3, 2), 1) == List(1, 2))
   }
 }
