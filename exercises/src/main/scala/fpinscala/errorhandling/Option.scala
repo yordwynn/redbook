e8641dec912db131d48f6d871e00a3657891b674
yordwynn <yurij.a.egorov@gmail.com>
2021-11-16 01:04:50 +0500

    Ex 4.1

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,31 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = {
+    this match {
+      case Some(get) => Some(f(get))
+      case None => None
+    }
+  }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B = {
+    this match {
+      case Some(get) => get
+      case None => default
+    }
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = {
+    map(f).getOrElse(None)
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] = {
+    map(_ => this).getOrElse(ob)
+  }
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = {
+    flatMap(v => if (f(v)) this else None)
+  }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
---
+++ exercises/src/test/scala/fpinscala/errorhandling/OptionTest.scala
@@ -0,0 +1,56 @@
+package fpinscala.errorhandling
+
+import fpinscala.errorhandling._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class OptionTest extends AnyFlatSpec {
+  "option" should "handle map" in {
+    assert(Some(1).map(_ * 2) == Some(2))
+    assert((None: Option[Int]).map(_ * 2) == None)
+  }
+
+  "option" should "handle flatMap" in {
+    assert(Some(1).flatMap(a => Some(a * 2)) == Some(2))
+    assert((None: Option[Int]).flatMap(a => Some(a * 2)) == None)
+  }
+
+  "option" should "handle getOrElse" in {
+    assert(Some(32).getOrElse(0) == 32)
+    assert(None.getOrElse(42) == 42)
+  }
+
+  "option" should "handle orElse" in {
+    assert(Some(42).orElse(None) == Some(42))
+    assert(Some(42).orElse(Some(10)) == Some(42))
+    assert(None.orElse(None) == None)
+    assert(None.orElse(Some(10)) == Some(10))
+  }
+
+  "option" should "handle filter" in {
+    assert(Some(20).filter(a => a % 2 == 0) == Some(20))
+    assert(Some(21).filter(a => a % 2 == 0) == None)
+    assert((None: Option[Int]).filter(a => a % 2 == 0) == None)
+  }
+
+//   "option" should "handle variance" in {
+//     assert(Option.variance(Seq(1, 2, 3, 4)) == Some(1.25))
+//   }
+
+//   "option" should "handle map2" in {
+//     assert(Option.map2(Some(1), Some(2))(_ + _) == Some(3))
+//     assert(Option.map2(None: Option[Int], Some(2))(_ + _) == None)
+//     assert(Option.map2(Some(1), None: Option[Int])(_ + _) == None)
+//     assert(Option.map2(Some(1), None: Option[Int])(_ + _) == None)
+//     assert(Option.map2(None: Option[Int], None: Option[Int])(_ + _) == None)
+//   }
+
+//   "option" should "handle sequencce" in {
+//     assert(Option.sequence(List(Some(1), None, Some(2))) == None)
+//     assert(Option.sequence(List(Some(1), Some(2))) == Some(List(1, 2)))
+//   }
+
+//   "option" should "handle traverse" in {
+//     assert(Option.traverse(List(0, 1, 2))(x => Option.Try(x + 1)) == Some(List(1, 2, 3)))
+//     assert(Option.traverse(List(0, 1, 2))(x => Option.Try(1 / x)) == None)
+//   }
+}
