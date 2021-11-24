a480f8b368b53d4f9d34d5f406825966621b4ff3
yordwynn <yurij.a.egorov@gmail.com>
2021-11-24 22:30:01 +0500

    Ex 5.1

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -2,21 +2,41 @@ package fpinscala.laziness
 
 import Stream._
 trait Stream[+A] {
-
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+  def foldRight[B](
+    z: => B
+  )(f: (A, => B) => B
+  ): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) =>
+        f(
+          h(),
+          t().foldRight(z)(f),
+        ) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
-    foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)(
+      (a, b) => p(a) || b
+    ) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
   final def find(f: A => Boolean): Option[A] = this match {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
+
+  def toList: List[A] = {
+    def go(source: Stream[A], res: List[A]): List[A] = {
+      source match {
+        case Cons(h, t) => go(t(), h() +: res)
+        case Empty => res
+      }
+    }
+
+    go(this, List.empty).reverse
+  }
+
   def take(n: Int): Stream[A] = ???
 
   def drop(n: Int): Stream[A] = ???
@@ -45,7 +65,7 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
---
+++ exercises/src/test/scala/fpinscala/laziness/StreamTest.scala
@@ -0,0 +1,163 @@
+package fpinscala.laziness
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class StreamTest extends AnyFlatSpec {
+  "Stream" should "handle toList" in {
+    val stream = Stream(1, 2, 3, 4)
+    assert(stream.toList == List(1, 2, 3, 4))
+  }
+
+//   "Stream" should "handle take N elements" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     assert(stream.take(3).toList == List(1, 2, 3))
+//   }
+
+//   "Stream" should "handle take N elements via unfold" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     assert(stream.takeViaUnfold(3).toList == List(1, 2, 3))
+//   }
+
+//   "Stream" should "handle drop N elements" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     assert(stream.drop(3).toList == List(4, 5))
+//   }
+
+//   "Stream" should "handle takeWhile" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     assert(stream.takeWhile(a => a != 3).toList == List(1, 2))
+//   }
+
+//   "Stream" should "handle takeWhile via foldRight" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     assert(stream.takeWhileViaFoldRight(a => a != 3).toList == List(1, 2))
+//   }
+
+//   "Stream" should "handle takeWhile via unfold" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     assert(stream.takeWhileViaUnfold(a => a != 3).toList == List(1, 2))
+//   }
+
+//   "Stream" should "handle forAll" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     assert(stream.forAll(a => a < 10))
+//     assert(!stream.forAll(a => a != 3))
+//   }
+
+//   "Stream" should "handle headOption via foldRight" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     val emptyStream = Stream.empty
+//     assert(stream.headOption.contains(1))
+//     assert(emptyStream.headOption.isEmpty)
+//   }
+
+//   "Stream" should "handle map via foldRight" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     val emptyStream = Stream.empty[Int]
+//     assert(stream.map(_ + 1).toList == List(2, 3, 4, 5, 6))
+//     assert(emptyStream.map(_ + 1).toList == List.empty)
+//   }
+
+//   "Stream" should "handle map2 via unfold" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     val emptyStream = Stream.empty[Int]
+//     assert(stream.mapViaUnfold(_ + 1).toList == List(2, 3, 4, 5, 6))
+//     assert(emptyStream.mapViaUnfold(_ + 1).toList == List.empty)
+//   }
+
+//   "Stream" should "handle filter via foldRight" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     val emptyStream = Stream.empty[Int]
+//     assert(stream.filter(_ % 2 == 0).toList == List(2, 4))
+//     assert(emptyStream.filter(_ % 2 == 0).toList == List.empty)
+//   }
+
+//   "Stream" should "handle append via foldRight" in {
+//     val stream = Stream(1, 2, 3, 4, 5)
+//     val emptyStream = Stream.empty[Int]
+//     assert(stream.append(Stream.apply(6)).toList == List(1, 2, 3, 4, 5, 6))
+//     assert(emptyStream.append(Stream.apply(1)).toList == List(1))
+//   }
+
+//   "Stream" should "handle flatMap via foldRight" in {
+//     val stream = Stream(1, 2, 3)
+//     val emptyStream = Stream.empty[Int]
+//     assert(stream.flatMap(x => Stream.apply(x, x)).toList == List(1, 1, 2, 2, 3, 3))
+//     assert(emptyStream.flatMap(x => Stream.apply(x, x)).toList == List.empty)
+//   }
+
+//   "Stream" should "handle constant function" in {
+//     assert(Stream.constant(3).take(2).toList == List(3, 3))
+//   }
+
+//   "Stream" should "handle constant via unfold" in {
+//     assert(Stream.constantViaUnfold(3).take(2).toList == List(3, 3))
+//   }
+
+//   "Stream" should "handle from function" in {
+//     assert(Stream.from(3).take(2).toList == List(3, 4))
+//   }
+
+//   "Stream" should "handle from via unfold" in {
+//     assert(Stream.fromViaUnfold(3).take(2).toList == List(3, 4))
+//   }
+
+//   "Stream" should "handle fibs function" in {
+//     assert(Stream.fibs.take(6).toList == List(0, 1, 1, 2, 3, 5))
+//   }
+
+//   "Stream" should "handle fibs via unfold" in {
+//     assert(Stream.fibsViaUnfold.take(6).toList == List(0, 1, 1, 2, 3, 5))
+//   }
+
+//   "Stream" should "handle unfold" in {
+//     val infiniteAcual = Stream.unfold(1)(s => Some(s -> s)).take(3).toList
+//     val infiniteExpected = List(1, 1, 1)
+//     assert(infiniteExpected == infiniteAcual)
+
+//     val finiteActual = Stream.unfold(0)(s => if (s < 5) Some((s, s + 1)) else None).toList
+//     val finiteExpected = List(0, 1, 2, 3, 4)
+//     assert(finiteExpected == finiteActual)
+//   }
+
+//   "Stream" should "handle ones via unfold" in {
+//     assert(Stream.onesViaUnfold.take(2).toList == List(1, 1))
+//   }
+
+//   "Stream" should "handle zipWith" in {
+//     val stream1 = Stream(1, 2, 3)
+//     val stream2 = Stream(1, 2, 3, 4)
+//     val expected = List(2, 4, 6)
+//     assert(stream1.zipWith(stream2)((a, b) => a + b).toList == expected)
+//   }
+
+//   "Stream" should "handle zipAll" in {
+//     val stream1 = Stream(1, 2, 3)
+//     val stream2 = Stream(1, 2, 3, 4)
+//     val expected = List((Some(1), Some(1)), (Some(2), Some(2)), (Some(3), Some(3)), (None, Some(4)))
+//     assert(stream1.zipAll(stream2).toList == expected)
+//   }
+
+//   "Stream" should "handle startsWith" in {
+//     val stream1 = Stream(1, 2, 3)
+//     val stream2 = Stream(2, 3)
+//     val stream3 = Stream(1, 2)
+//     val stream4 = Stream(1, 2, 3, 4)
+
+//     assert(!stream1.startsWith(stream2))
+//     assert(stream1.startsWith(stream3))
+//     assert(!stream1.startsWith(stream4))
+//   }
+
+//   "Stream" should "handle tails" in {
+//     val stream1 = Stream(1, 2, 3)
+//     val expected = List(List(1, 2, 3), List(2, 3), List(3), List())
+//     assert(stream1.tails.map(s => s.toList).toList == expected)
+//   }
+
+//   "Stream" should "handle scanRight" in {
+//     val stream1 = Stream(1, 2, 3)
+//     val expected = List(6, 5, 3, 0)
+//     assert(stream1.scanRight(0)(_ + _).toList == expected)
+//   }
+}
