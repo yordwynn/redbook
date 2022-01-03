8a42275b38832a7311a538164de791190a465a2c
yordwynn <yurij.a.egorov@gmail.com>
2022-01-04 01:44:53 +0500

    Ex 7.5

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -79,6 +79,9 @@ object Par {
 
   def sortPar(parList: Par[List[Int]]) = map(parList)(_.sorted)
 
+  def sequence[A](pars: List[Par[A]]): Par[List[A]] =
+    pars.foldRight(unit(List.empty[A]))((b, p) => map2(b, p)((a, b) => a +: b))
+
   def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean =
     p(e).get == p2(e).get
 
